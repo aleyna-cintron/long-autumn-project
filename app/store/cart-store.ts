446093a6ac274057bd8c6c4ac29d15f33107ab1b
aleyna-cintron <aleynatcintron@gmail.com>
2025-10-20 20:19:28 -0400

    created cato on stripe

--- app/store/cart-store.ts
+++ app/store/cart-store.ts
@@ -4,3 +4,14 @@
 
 // define what an item is: it requires the defined cart item, it also requires a function
 
+import { create } from 'zustand'
+
+const useCartStore = create((set) => ({
+  items: 0,
+
+  addItem: () => set((state: Product: product) => ({ item: state.item + 1 })),
+
+//   removeAllItems: () => set({ item: 0 }),
+
+//   updateBears: (newBears) => set({ bears: newBears }),
+}))

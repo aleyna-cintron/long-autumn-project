20ab615f7524ffbe35414b0e0a48c5322968acbc
CintronAleyna-FS <ACintron1@student.fullsail.edu>
2026-02-04 13:02:32 -0500

    instagram section removed from gallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,11 +1,10 @@
 import GalleryGrid from "../components/gallery/GalleryGrid";
 import Quote from "../components/gallery/Quote";
-import Socials from "../components/gallery/Socials";
 import PageHeader from "../components/ui/PageHeader";
 
 export default function Gallery() {
   return (
-    <div className="w-full min-h-screen text-text-primary md:pt-20 lg:pt-24 pb-20">
+    <div className="w-full min-h-screen text-text-primary md:pt-20 lg:pt-24">
       <div className="absolute inset-0" />
       <PageHeader
         title="Gallery"
@@ -14,7 +13,6 @@ export default function Gallery() {
       {/* Gallery */}
       <GalleryGrid></GalleryGrid>
       <Quote></Quote>
-      <Socials></Socials>
     </div>
   );
 }

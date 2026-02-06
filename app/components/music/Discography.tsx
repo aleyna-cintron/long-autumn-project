074776f7544e730677665d10fc9d84a7786efcc3
CintronAleyna-FS <ACintron1@student.fullsail.edu>
2026-02-06 04:04:22 -0500

    added padding to text

--- app/components/music/Discography.tsx
+++ app/components/music/Discography.tsx
@@ -12,11 +12,11 @@ export default function Discography({ currentEP, onSelectEP }: DiscographyProps)
     <section className="relative w-full py-16 min-h-screen xl:min-h-0 xl:py-8">
       <div className="mx-auto">
         <div className="flex flex-col items-center md:mt-40">
-          <h3 className="text-4xl md:text-5xl lg:text-6xl 2xl:text-6xl 3xl:text-7xl 4xl:text-8xl font-bold text-center text-text-primary">
+          <h3 className="p-2 lg:py-2 text-4xl md:text-5xl lg:text-6xl 2xl:text-6xl 3xl:text-7xl 4xl:text-8xl font-bold text-center text-text-primary">
             Discography
           </h3>
 
-          <p className="max-w-3xl 3xl:max-w-4xl text-center text-base md:text-lg lg:text-xl 2xl:text-xl 3xl:text-2xl 4xl:text-2xl text-text-secondary leading-relaxed mb-16">
+          <p className="p-4 max-w-3xl 3xl:max-w-4xl text-center text-base md:text-lg lg:text-xl 2xl:text-xl 3xl:text-2xl 4xl:text-2xl text-text-secondary leading-relaxed mb-16">
             Each release shows where we were at the time —
             written in garages, played in bars, and built with brothers.
             Not by blood, just by showing up.

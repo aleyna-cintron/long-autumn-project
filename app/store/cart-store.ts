a14d4b6e77fd7e866d4a82cf1815ceae14c8e654
CintronAleyna-FS <ACintron1@student.fullsail.edu>
2026-02-07 16:19:40 -0500

    cart persists after refresh

--- app/store/cart-store.ts
+++ app/store/cart-store.ts
@@ -1,5 +1,6 @@
 // stores/cart-store.ts
 import { createStore } from 'zustand/vanilla'
+import { persist } from 'zustand/middleware'
 
 export type CartItem = {
   id: string;
@@ -13,31 +14,36 @@ export type CartStore = {
   items: CartItem[]
   addItem: (item: CartItem) => void
   removeItem: (id: string) => void
-  decrementItem: (id: string) => void 
+  decrementItem: (id: string) => void
 }
 
 export const createCartStore = () =>
-  createStore<CartStore>((set) => ({
-    items: [],
-    addItem: (item: CartItem) =>
-      set((state) => {
-        const existing = state.items.find((i) => i.id === item.id);
-        if (existing) {
-          return {
-            items: state.items.map((i) =>
-              i.id === item.id ? { ...i, quantity: i.quantity + item.quantity } : i
-            ),
-          };
-        } else {
-          return { items: [...state.items, item] };
-        }
+  createStore<CartStore>()(
+    persist(
+      (set) => ({
+        items: [],
+        addItem: (item: CartItem) =>
+          set((state) => {
+            const existing = state.items.find((i) => i.id === item.id);
+            if (existing) {
+              return {
+                items: state.items.map((i) =>
+                  i.id === item.id ? { ...i, quantity: i.quantity + item.quantity } : i
+                ),
+              };
+            } else {
+              return { items: [...state.items, item] };
+            }
+          }),
+        removeItem: (id) =>
+          set((state) => ({ items: state.items.filter((i) => i.id !== id) })),
+        decrementItem: (id) =>
+          set((state) => ({
+            items: state.items.map(i => i.id === id ? {...i, quantity: i.quantity-1} :i).filter((i) =>i.quantity > 0)
+          }))
       }),
-    removeItem: (id) =>
-      set((state) => ({ items: state.items.filter((i) => i.id !== id) })),
-    decrementItem: (id) =>
-      set((state) => ({
-        items: state.items.map(i => i.id === id ? {...i, quantity: i.quantity-1} :i).filter((i) =>i.quantity > 0)
-      }))
-  }))
+      { name: 'cart-storage' }
+    )
+  )
 
   
